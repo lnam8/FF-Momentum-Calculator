a0beca057e65cd399b12fe437505f9f9746120a8
Rahim Lalani <rahim@Rahims-MBP.attlocal.net>
2023-10-19 13:02:37 -0500

    modified script to pull for a single week only and added week 6 data and top performers

--- scripts/get_weekly_statistics/weekly_data.py
+++ scripts/get_weekly_statistics/weekly_data.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import argparse
 
 # API URL to make request to to pull in weekly data
 url = 'https://api.sleeper.app/v1/stats/nfl/regular/2023/'
@@ -22,83 +23,83 @@ te_columns = ['Rank', 'player_name', 'receptions', 'targets', 'receiving_yards',
 
 if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(description="A script that gathers weekly Fantasy Football Statistics")
+    parser.add_argument("--week", type=int, help="Week as an integer ex. 1")
+    args = parser.parse_args()
+    week = args.week
+
     # Gathering the required weekly statistics data
     data = []
     top_performers_df = pd.DataFrame()
-    for week in range(1, 19):
-        r = requests.get(url + '{}'.format(week))
-        d = r.json()
-        if d == {}:
-            print('Week {} data not yet available'.format(week))
-            continue
+
+    r = requests.get(url + '{}'.format(week))
+    d = r.json()
+    
+    # Appends weekly data for the requested 6 players above
+    for player in players:
+        try:
+            d_info = d[player['id']]
+            data.append({
+                'player_name': player['name'],
+                'player_position': player['position'],
+                'player_team': player['team'],
+                'week': int(week),
+                'receptions': int(d_info.get('rec', 0)),
+                'targets': int(d_info.get('rec_tgt', 0)),
+                'receiving_yards': int(d_info.get('rec_yd', 0)),
+                'receiving_yards_per_reception': float(d_info.get('rec_ypr', 0)),
+                'receiving_touchdowns': int(d_info.get('rec_td', 0)),
+                'rushing_attempts': int(d_info.get('rush_att', 0)),
+                'rushing_yards': int(d_info.get('rush_yd', 0)),
+                'rushing_yards_per_attempt': float(d_info.get('rush_ypa', 0)),
+                'rushing_touchdowns': int(d_info.get('rush_td', 0)),
+                'standard_points': float(d_info.get('pts_std', 0)),
+                'half_ppr_points': float(d_info.get('pts_half_ppr', 0)),
+                'ppr_points': float(d_info.get('pts_ppr', 0))
+            })
+        except KeyError as ke:
+            print('Data for {} in Week {} not available yet'.format(player['name'], week))
+    print('Added weekly data for requested players for Week {}.'.format(week))
+    
+    # Appends weekly data for the top WR, RB, and TE performers
+    wr = ['https://www.fantasypros.com/nfl/stats/wr.php?week={}&scoring=HALF&range=week'.format(week), 'WR', wr_columns]
+    rb = ['https://www.fantasypros.com/nfl/stats/rb.php?week={}&scoring=HALF&range=week'.format(week), 'RB', rb_columns]
+    te = ['https://www.fantasypros.com/nfl/stats/te.php?week={}&scoring=HALF&range=week'.format(week), 'TE', te_columns]
+    position_groups = [wr, rb, te]
+
+    # Loop through position groups an pull top performers names
+    for group in position_groups:
+        fantasy_pros_url = group[0]
+        position = group[1]
+        df_columns = group[2]
+
+        # Read in the top performing players per position
+        df = pd.read_html(fantasy_pros_url)[0]
+
+        # Format data so that it is similar to JSON above
+        df.columns = df_columns
+        df['player_team'] = df.player_name.apply(lambda x: ' '.join(x.split()[-1:])[1:-1])
+        df['player_name'] = df.player_name.apply(lambda x: ' '.join(x.split()[:-1]))
+        df['player_position'] = position
+        df['week'] = week
+        df['standard_points'] = float(0)
+        df['ppr_points'] = float(0)
+        if position in ['WR', 'TE']:
+            df['rushing_yards_per_attempt'] = df['rushing_yards'].div(df['rushing_attempts'], axis=0).round(1).fillna(0)
+        df = df[['player_name', 'player_position', 'player_team', 'week', 'receptions', 'targets', 'receiving_yards', 'receiving_yards_per_reception', 'receiving_touchdowns', 'rushing_attempts', 'rushing_yards', 'rushing_yards_per_attempt', 'rushing_touchdowns', 'standard_points', 'half_ppr_points', 'ppr_points']]
         
-        # Appends weekly data for the requested 6 players above
-        for player in players:
-            try:
-                d_info = d[player['id']]
-                data.append({
-                    'player_name': player['name'],
-                    'player_position': player['position'],
-                    'player_team': player['team'],
-                    'week': int(week),
-                    'receptions': int(d_info.get('rec', 0)),
-                    'targets': int(d_info.get('rec_tgt', 0)),
-                    'receiving_yards': int(d_info.get('rec_yd', 0)),
-                    'receiving_yards_per_reception': float(d_info.get('rec_ypr', 0)),
-                    'receiving_touchdowns': int(d_info.get('rec_td', 0)),
-                    'rushing_attempts': int(d_info.get('rush_att', 0)),
-                    'rushing_yards': int(d_info.get('rush_yd', 0)),
-                    'rushing_yards_per_attempt': float(d_info.get('rush_ypa', 0)),
-                    'rushing_touchdowns': int(d_info.get('rush_td', 0)),
-                    'standard_points': float(d_info.get('pts_std', 0)),
-                    'half_ppr_points': float(d_info.get('pts_half_ppr', 0)),
-                    'ppr_points': float(d_info.get('pts_ppr', 0))
-                })
-            except KeyError as ke:
-                print('Data for {} in Week {} not available yet'.format(player['name'], week))
-        print('Added weekly data for requested players for Week {}.'.format(week))
-        
-        # Appends weekly data for the top WR, RB, and TE performers
-        wr = ['https://www.fantasypros.com/nfl/stats/wr.php?week={}&scoring=HALF&range=week'.format(week), 'WR', wr_columns]
-        rb = ['https://www.fantasypros.com/nfl/stats/rb.php?week={}&scoring=HALF&range=week'.format(week), 'RB', rb_columns]
-        te = ['https://www.fantasypros.com/nfl/stats/te.php?week={}&scoring=HALF&range=week'.format(week), 'TE', te_columns]
-        position_groups = [wr, rb, te]
+        # Retrieving only records that have fantasy points scored in that week
+        df = df[df.half_ppr_points > 0]
 
-        # Loop through position groups an pull top performers names
-        for group in position_groups:
-            fantasy_pros_url = group[0]
-            position = group[1]
-            df_columns = group[2]
-
-            # Read in the top performing players per position
-            df = pd.read_html(fantasy_pros_url)[0]
-
-            # Format data so that it is similar to JSON above
-            df.columns = df_columns
-            df['player_team'] = df.player_name.apply(lambda x: ' '.join(x.split()[-1:])[1:-1])
-            df['player_name'] = df.player_name.apply(lambda x: ' '.join(x.split()[:-1]))
-            df['player_position'] = position
-            df['week'] = week
-            df['standard_points'] = float(0)
-            df['ppr_points'] = float(0)
-            if position in ['WR', 'TE']:
-                df['rushing_yards_per_attempt'] = df['rushing_yards'].div(df['rushing_attempts'], axis=0).round(1).fillna(0)
-            df = df[['player_name', 'player_position', 'player_team', 'week', 'receptions', 'targets', 'receiving_yards', 'receiving_yards_per_reception', 'receiving_touchdowns', 'rushing_attempts', 'rushing_yards', 'rushing_yards_per_attempt', 'rushing_touchdowns', 'standard_points', 'half_ppr_points', 'ppr_points']]
-            
-            # Retrieving only records that have fantasy points scored in that week
-            df = df[df.half_ppr_points > 0]
-
-            top_performers_df = pd.concat([top_performers_df, df])
+        top_performers_df = pd.concat([top_performers_df, df])
 
     pdf = pd.DataFrame(data)
     pdf = pd.concat([pdf, top_performers_df])
 
     # Appending any new records to the CSV
-    weekly_data = pd.read_csv('weekly_data.csv')
-    complement = pd.concat([weekly_data, pdf], ignore_index=True)
-    complement.drop_duplicates(inplace=True, keep=False)
-    if len(complement) != 0:
-        print('Adding {} records to `weekly_data.csv`'.format(len(complement)))
-        complement.to_csv('weekly_data.csv', mode='a', index=False, header=False)
+    if len(pdf) != 0:
+        print('Adding {} records to `weekly_data.csv`'.format(len(pdf)))
+        print(pdf)
+        pdf.to_csv('weekly_data.csv', mode='a', index=False, header=False)
     else:
         print('No new records to add')
