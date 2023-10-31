5e7ac1ac366d0f5b8d7678b02e7d353af9ddbad4
Mackowski, Jacob Andrew <jmackowski3@gatech.edu>
2023-10-31 14:50:25 -0400

    increase speed, pull all from db, and added the hist

--- rough_draft_code/rough_draft_cluster_dataset.py
+++ rough_draft_code/rough_draft_cluster_dataset.py
@@ -4,88 +4,143 @@ import pandas as pd
 import os 
 import glob
 import sqlite3          
-WEEK_OF_THE_SEASON = 6
+WEEK_OF_THE_SEASON = 8
+CURRENT_FOOTBALL_YEAR = 2023
+THESE_PLAYERS = ['Puka Nakua','Josh Jacobs','Kyle Pitts','Zach Ertz','Raheem Mostert','Calvin Ridley'] 
+
+
 
 #In[]:
 conn = sqlite3.connect("C:\\Users\\15869\\Desktop\\FF_Teamwork\\FF-Momentum-Calculator\\db\\ff_momentum.db")
 sql_query = """SELECT name FROM sqlite_master 
     WHERE type='table';"""
- 
-    # Creating cursor object using connection object
 cursor = conn.cursor()
-     
-    # executing our sql query
+ 
 cursor.execute(sql_query)
-print("List of tables\n")
-     
-    # printing all tables list
-
 tables = cursor.fetchall()
 
 #In[]:
 
+"""
+###########################################################################################
+Loading the data needed for the pull
+data_weekly = all past weekly number data
+cdata_weekly = Current Year ( defined above) data pulled for
 
-# for item in tables:
-
-#     print(item[0])
-#     temp_query = """select * FROM """ +str(item[0]) +    "Limit 3"
-#     #print(temp_query)
-#     cursor.execute(sql_query)
-#     print(cursor.fetchall())
-
-#In[]:
-
-weekly_stats_query = """select * FROM """ +"historical_weekly_stats " 
+"""
+weekly_stats_query = """select player_name ,
+            player_position,
+            player_team ,
+            year, 
+            week ,
+            receptions ,
+            targets ,
+            
+           
+            
+            half_ppr_points 
+              FROM """ +"historical_weekly_stats where year>2017 " 
 cursor.execute(weekly_stats_query)
 data_weekly = cursor.fetchall()
 
 cols = ['player_name' ,
             'player_position',
             'player_team' ,
+            'year', 
             'week' ,
             'receptions' ,
             'targets' ,
-            'receiving_yards' ,
-            'receiving_yards_per_reception' ,
-            'receiving_touchdowns' ,
-            'rushing_attempts' ,
-            'rushing_yards' ,
-            'rushing_yards_per_attempt' ,
-            'rushing_touchdowns' ,
-            'standard_points' ,
-            'half_ppr_points' ,
-            'ppr_points' ]
+           #'receiving_yards' ,
+            # 'receiving_yards_per_reception' ,
+            # 'receiving_touchdowns' ,
+            # 'rushing_attempts' ,
+            # 'rushing_yards' ,
+            # 'rushing_yards_per_attempt' ,
+            # 'rushing_touchdowns' ,
+            
+            'half_ppr_points' 
+            
+            ]
 df_data_weekly = pd.DataFrame(data_weekly,columns = cols)
-df_data_weekly['year_shift']= df_data_weekly['week'].shift(1)
-df_data_weekly['new_year'] = 1 * (df_data_weekly['year_shift'] != df_data_weekly['week']) * (df_data_weekly['week'] == 1)
-df_data_weekly['what_year'] = df_data_weekly['new_year'].cumsum() 
+
+
 
 #In[]:
+cweekly_stats_query = """select DISTINCT 
+            player_name ,
+            player_position,
+            player_team ,
+            week, 
+            receptions ,
+            targets ,
+           
+            
+            half_ppr_points
+             FROM """ +"weekly_stats limit 5000 " 
+cursor.execute(cweekly_stats_query)
+cdata_weekly = cursor.fetchall()
 
-current_weekly_data = pd.read_csv('C:\\Users\\15869\\Desktop\\FF_Teamwork\\FF-Momentum-Calculator\\scripts\\get_weekly_statistics\\weekly_data.csv')
-current_weekly_data['what_year'] = 'CURRENT'
 
-df_data_weekly = pd.concat([df_data_weekly,current_weekly_data]).reset_index(drop=True)
+cols2 = ['player_name' ,
+            'player_position',
+            'player_team' ,
+            'week', 
+            'receptions' ,
+            'targets' ,
+            #'receiving_yards' ,
+            #'receiving_yards_per_reception' ,
+            # 'receiving_touchdowns' ,
+            # 'rushing_attempts' ,
+            # 'rushing_yards' ,
+            # 'rushing_yards_per_attempt' ,
+            # 'rushing_touchdowns' ,
+            
+            'half_ppr_points' 
+            
+            ]
+cdf_data_weekly = pd.DataFrame(cdata_weekly,columns = cols2)
+cdf_data_weekly['year'] = CURRENT_FOOTBALL_YEAR
 
+df_data_weekly = pd.concat([df_data_weekly,cdf_data_weekly]).reset_index(drop=True)
+
+df_data_weekly['join_column'] = df_data_weekly.apply(lambda x:  str(x['player_name'] ) +" " + str(x['year'] ) + " " + str(x['week'] ),axis = 1) 
+#In[]:
+
+
+madden_weekly = """select * FROM """ +"madden_weekly  limit 5000 " 
+data = cursor.execute(madden_weekly)
+columns_in_madden = []
+for column in data.description: 
+    columns_in_madden.append(str(column[0]))
+madden_weekly_data = cursor.fetchall()
+
+madden_weekly_data = pd.DataFrame(madden_weekly_data,columns = columns_in_madden)
+madden_weekly_data['join_column'] = madden_weekly_data.apply( lambda x: str(x['firstName']) + \
+                                    " " + str(x['lastName']) + " " + \
+                                    str(x['year']) +" " +  str( str(x['week']) ),axis  = 1)
 
 #In[]:
 if conn:
         # using close() method, we will close 
         # the connection
         conn.close()
+
+
+
+
 #In[]:
 import tqdm
-years =  df_data_weekly['what_year'].unique()
+years =  df_data_weekly['year'].unique()
 all_players =  df_data_weekly['player_name'].unique()
 each_player_by_year = {}
 for x in tqdm.tqdm(years):
     print(x)
 
 #In[]:
-for y in tqdm.tqdm(years):
+'''for y in tqdm.tqdm(years):
       for player in all_players:
             #print(y,player)
-            temp_df = df_data_weekly[df_data_weekly['what_year'] == y]
+            temp_df = df_data_weekly[df_data_weekly['year'] == y]
 
             try: 
                 one_player = temp_df[temp_df['player_name'] == player]
@@ -105,22 +160,25 @@ for y in tqdm.tqdm(years):
                 each_player_by_year[string_concat] = (temp_list) 
             except:
                   pass # print('didnt play this year')
+output_df_int_cluster = pd.DataFrame.from_dict(each_player_by_year).T
 
-
-
-
+'''
+piv_data = df_data_weekly.pivot(index= ['year','player_name','player_position'],columns = 'week',values = 'half_ppr_points').fillna(0).reset_index()
+piv_data['index'] = piv_data.apply(lambda x: x['player_name'] + " " + str(x['year']), axis =1)
+piv_data.drop(columns=['year','player_name','player_position'],inplace= True)
+piv_data.set_index('index',inplace=True, drop=True)
 
 output_df_int_cluster = pd.DataFrame.from_dict(each_player_by_year).T
 
 #In[]:
-good_indexes =  output_df_int_cluster.sum(axis = 1) > 10
+good_indexes =  piv_data.sum(axis = 1) > 10
 
-output_df_int_cluster = output_df_int_cluster[good_indexes]
+piv_data = piv_data[good_indexes]
 #In[]:
 
-COLUMNS_NEEDED = range(0,WEEK_OF_THE_SEASON - 1)
+COLUMNS_NEEDED = range(1,WEEK_OF_THE_SEASON )
 
-modeling_output_df_int_cluster  = output_df_int_cluster[COLUMNS_NEEDED]
+modeling_output_df_int_cluster  = piv_data[COLUMNS_NEEDED]
 
 from sklearn.neighbors import NearestNeighbors
 for i in tqdm.tqdm([1]):
@@ -133,19 +191,20 @@ for i in tqdm.tqdm([1]):
 import numpy as np
 import matplotlib.pyplot as plt
 
-new_df = modeling_output_df_int_cluster.reset_index()
-future_df = output_df_int_cluster.reset_index()
+new_df = modeling_output_df_int_cluster.reset_index('index')
+future_df = piv_data.reset_index('index')
 
 
 """This section to be removed for real world application"""
-TEMP_PROVE_POINT_COL = range(max(modeling_output_df_int_cluster.columns),max(output_df_int_cluster.columns)+1)
-future_output_df_int_cluster  = output_df_int_cluster[TEMP_PROVE_POINT_COL]
+TEMP_PROVE_POINT_COL = range(max(modeling_output_df_int_cluster.columns),max(piv_data.columns)+1)
+future_output_df_int_cluster  = piv_data[TEMP_PROVE_POINT_COL]
 temp_df = future_output_df_int_cluster.reset_index()
 
 
 
 
-def findsuccess(id):
+def findsuccess(id,WEEK_OF_THE_SEASON):
+    hist_data = []
     labels = []
     plt.figure(figsize=(15,5))
     index_for_nn = np.where(B[:][id] ==1)[0]
@@ -157,9 +216,14 @@ def findsuccess(id):
     for item in index_for_nn:
         if item != id: 
             print('Neighbors of id', new_df.iloc[item]['index'])
-            plt.scatter(output_df_int_cluster.columns,list(future_df.iloc[item])[1:])
+            plt.scatter(piv_data.columns,list(future_df.iloc[item])[1:])
+
             labels.append(np.array(future_df.iloc[item])[0])
+            if sum(list(future_df.iloc[item])[WEEK_OF_THE_SEASON:]) > 5:
+                for s in list(future_df.iloc[item])[1:]:
+                    hist_data.append(s)
     plt.legend(labels)
+    return hist_data
 
 
 
@@ -170,17 +234,24 @@ def findsuccess(id):
 #In[]:
 import re
 list_of_people = []
-these_players = ['Puka Nakua CURRENT','Josh Jacobs CURRENT','Kyle Pitts CURRENT','Zach Ertz CURRENT','Raheem Mostert CURRENT','Calvin Ridley CURRENT'] 
-for person in these_players: 
-    for item in modeling_output_df_int_cluster.index:
 
-        if re.findall(person,item) :
+for person in THESE_PLAYERS: 
+    for item in modeling_output_df_int_cluster.index:
+        person_year = person + " " +  str(CURRENT_FOOTBALL_YEAR)
+        if re.findall(person_year,item) :
+            
             temps = re.findall(person,item)
             for k in temps:
-                if re.findall('CURR',k):
+            
 
-                    list_of_people.append(new_df[new_df['index']== item ].index[0])
+                list_of_people.append(new_df[new_df['index']== item ].index[0])
 
 
 for number_id in list_of_people:
-     findsuccess(number_id)
+
+    hist_data = findsuccess(number_id,WEEK_OF_THE_SEASON)
+    plt.figure(figsize=(5,5))
+    plt.hist(list(hist_data))
+
+#TODO Madden Data to be able to have the option to fiter the Random Forest by WR/ other
+#Currently looks at only the points not the points vs hard opponents
