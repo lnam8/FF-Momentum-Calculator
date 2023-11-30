440715cae3f75781e9b051d8477d9a1f28ee4f82
eitokazu <59630089+eitokazu@users.noreply.github.com>
2023-11-30 00:21:32 -0800

    Updated cusum plot
    Added player highlighting functionality and annotation

--- visualization/cusum_scripted-st.py
+++ visualization/cusum_scripted-st.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
 # from streamlit_echarts import st_echarts
 import plotly.express as px
+import plotly.graph_objects as go
 # import os
 
 # Set up database connections
@@ -71,6 +72,7 @@ def bubble_tables(df):
     bubble_std = bubbles['half_ppr_points'].std()
     bubble_avg_pos = bubbles.groupby('week')['pts_cumsum'].mean().reset_index()
     bubble_avg_pos.rename(columns={'pts_cumsum': 'bubble_avg'}, inplace=True)
+    bubble_avg_pos['bubble_avg'] = bubble_avg_pos['bubble_avg']+bubble_std
     return bubble_avg_pos, bubble_std
 
 # Join bubble info to main table and calculate breakout trigger
@@ -82,7 +84,7 @@ def breakouts(position_table, bubble_table, bubble_std):
     '''
     breakout_check = pd.merge(position_table, bubble_table, left_on='weeks_active', right_on='week', how='left')
     breakout_check['cumsum_diff'] = breakout_check['pts_cumsum']-breakout_check['bubble_avg']
-    breakout_check['breakout'] = np.where(breakout_check['cumsum_diff']>bubble_std, 1, 0)
+    breakout_check['breakout'] = np.where(breakout_check['cumsum_diff']>0, 1, 0)
     return breakout_check
 
 # RUN THROUGH
@@ -93,34 +95,98 @@ breakout_table = breakouts(position_table=position_table, bubble_table=bubble_av
 print(len(breakout_table))
 breakout_table.head(20)
 
-# test = position_table.loc[position_table['player_name']=='Puka Nacua']
 
-# test.head(20)
 
 df = breakout_table
+
+# Highlight Player
+
+players = sorted(list(df['player_name'].unique()))
+
+player_selection = st.selectbox(
+    'Select player to spotlight',
+    players,
+    index=None)
+
+st.write('You selected:', player_selection)
+
+st.markdown('''  
+    ### Fantasy Football Cumulative Data by Weeks Played
+    Players marked with :green[green] bubbles have surpassed the breakout threshold.  
+    Players with :gray[empty] bubbles are under the breakout threshold.   
+''')
+
+
 
 # VIZ
 
 
 # Preprocess data to create line dash styles
-df['line_dash'] = df['breakout'].apply(lambda x: 'dash' if x == 0 else 'solid')
+df['line_dash'] = df['breakout'].apply(lambda x: 'solid' if x == 1 else 'dash')
 
-# Group by player_name and plot
-fig = px.line(df, x='week_x', y='pts_cumsum', color='player_name',
-              line_dash='breakout', markers='breakout', 
-              title='Fantasy Football Data by Week',
-              category_orders={"player_name": sorted(df['player_name'].unique())})
+# Create a figure
+fig = go.Figure()
+
+sorted_player_names = sorted(df['player_name'].unique())
+
+# Group by player_name and add traces
+for player in sorted_player_names:
+    player_data = df[df['player_name'] == player]
+    marker_fill = ['green' if breakout == 1 else 'rgba(0, 0, 0, 0.2)' for breakout in player_data['breakout']]
+    marker_size = [8 if breakout == 1 else 4 for breakout in player_data['breakout']]
+    fig.add_trace(go.Scatter(x=player_data['weeks_active'], y=player_data['pts_cumsum'],
+                             mode='lines+markers', name=player, line=dict(dash='solid', width=0.8),
+                             marker=dict(size=marker_size, symbol='circle', line=dict(color='white', width=1),
+                                         opacity=1, line_color='black', color=marker_fill),
+                            text=[player_data['player_name']]))
 
 # Add average line
-fig.add_scatter(x=bubble_avg['week'], y=bubble_avg['bubble_avg'], mode='lines', 
-                line=dict(color='white'), name='Average', line_width=15, line_dash='dot')
+fig.add_trace(go.Scatter(x=bubble_avg['week'], y=bubble_avg['bubble_avg'], line_dash='dot',
+                         marker=dict(color='grey', size=8, symbol='circle'),
+                         mode='lines', line=dict(color='yellow', width=5), name='Threshold'))
+
+# Update if player selected for highlighting
+if player_selection:
+    player_select_data = df[df['player_name'] == player_selection]
+    selected_marker_fill = ['green' if breakout == 1 else 'rgba(0, 0, 0, 0.2)' for breakout in player_select_data['breakout']]
+    selected_marker_size = [12 if breakout == 1 else 8 for breakout in player_select_data['breakout']]
+    fig.add_trace(go.Scatter(x=player_select_data['weeks_active'], y=player_select_data['pts_cumsum'],
+                                mode='lines+markers', name=player_selection, line=dict(dash='solid', width=4, color='white'),
+                                marker=dict(size=selected_marker_size, symbol='circle', line=dict(color='white', width=1),
+                                            opacity=1, line_color='white', color=selected_marker_fill)))
+    
+    # Add green or red box annotation based on the breakout value of the last data point
+    last_point_breakout = player_select_data['breakout'].iloc[-1]
+    last_point_week = player_select_data['weeks_active'].iloc[-1]
+    box_color = 'green' if last_point_breakout == 1 else 'red'
+    box_text = 'START' if last_point_breakout == 1 else 'SIT'
+    fig.add_shape(
+        type='rect',
+        label=dict(
+            text=f'{player_selection}<br>{box_text}',
+            font=dict(
+                size=38,
+                family='Arial'
+            ),
+            textposition='middle center'
+        ),
+        x0=-0.4,
+        x1=max(df['weeks_active'])*3/5,
+        y0=max(df['pts_cumsum']),
+        y1=max(df['pts_cumsum'])*3/4,
+        fillcolor=box_color,
+        opacity=1,
+        layer='above',
+    )
 
 # Customize layout
-fig.update_layout(xaxis_title='Week Number', yaxis_title='Pts Cumsum', legend_title='Player Name', 
-                  height=1000, width=1000)
+fig.update_layout(xaxis_title='Weeks Played', yaxis_title='Cumulative Points Scored',
+                  legend_title='Player Name', height=1000, width=1000)
+
+fig.update_layout(
+    margin=dict(l=0, r=0, t=0, b=20),
+)
 
 # Display the chart using Streamlit
 st.plotly_chart(fig)
-
-
 
